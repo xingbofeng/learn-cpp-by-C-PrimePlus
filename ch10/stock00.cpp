deb0eeca067ba6df9a75e5dfa02b2431ecd64edc
FE_XingBofeng <encounterxingbofeng@outlook.com>
2017-10-26 01:05:13 +0800

    init

---
+++ ch10/stock00.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Stock {
+	private:
+		string company;
+		long shares;
+		double share_val;
+		double total_val;
+		void set_tot() {
+			total_val = shares * share_val;
+		}
+	public:
+		void acquire(const string& co, long n, double pr);
+		void buy(long num, double price);
+		void sell(long num, double price);
+		void update(double price);
+		void show();
+};
+
+int main() {
+	return 0;
+}
